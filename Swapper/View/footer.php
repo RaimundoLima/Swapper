864866e03cfb1c5397186ac30c5a0263d26459d2
Raimundo Lima <rai8991@hotmail.com>
2019-01-14 03:02:34 -0200

    commit

--- Swapper/View/footer.php
+++ Swapper/View/footer.php
@@ -2,7 +2,7 @@
     <div>
         <script src="View/js/jquery.js"></script>
         <script src="View/js/materialize.js"></script>
-        <script src="View/js/script.js"></script>
+        <script src="View/js/main.js"></script>
         <script src="View/js/chat.js"></script>
     </div>
 <footer>
