bf96d6c7be1455976c557d2ebcc40a1531bba471
Jacinto <luishjacinto98@gmail.com>
2019-01-14 04:37:25 -0200

    opa

--- Swapper/View/js/main.js
+++ Swapper/View/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $('.tabs').tabs();
     $('.tabs').tabs({ swipeable: true });
+  
 });
--- Swapper/View/main.php
+++ Swapper/View/main.php
@@ -17,9 +17,30 @@
                 </a></li>
             </ul>
         </div>
-        <div id="test1" class="col s12">Test 1</div>
-        <div id="test2" class="col s12">Test 2</div>
-        <div id="test3" class="col s12">Test 3</div>
+        <div id="test1" class="aba col s12">
+
+             <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                    <div class="card-image">
+                    <img src="/View/img/rai.jpg">
+                    <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                    <a href="#">This is a link</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+
+        </div>
+        <div id="test2" class="aba col s12">Test 2</div>
+        <div id="test3" class="aba col s12">Test 3</div>
     </div>
 
 </body>
