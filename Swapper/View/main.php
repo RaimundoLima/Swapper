ef3c6f84b1773f5673f4867af0f5cf2b7cdcb5d3
Jacinto <luishjacinto98@gmail.com>
2019-01-14 11:21:39 -0200

    Perfil atualizado

--- Swapper/View/main.php
+++ Swapper/View/main.php
@@ -4,25 +4,27 @@
 <body>
 
     <div class="row">
-        <div class="col s12">
-            <ul class="tabs">
-                <li class="tab col s3"><a href="#test1">
-                    <i class="medium material-icons">account_circle</i>
-                </a></li>
-                <li class="tab col s3"><a class="active" href="#test2">  
-                    <i class="medium material-icons">explore</i>
-                </a></li>
-                <li class="tab col s3"><a href="#test3">  
-                    <i class="medium material-icons">chat</i>
-                </a></li>
-            </ul>
-        </div>
         <div id="test1" class="aba col s12">
 
              <div class="row">
                 <div class="perfil_tab col s12">
                 
                 <h4 class="titulo_tab" >Perfil</h4>
+                <br>
+
+                <div class="col s1"></div>
+                <div class="perfil_dados col s10">
+                    
+                    <div class="pic">
+                        <img src="" alt="">
+                    </div>
+
+                    
+                    <h5 class="nome">Django</h5>
+                    <h6 class="cidade">Velho Oeste</h6>
+
+                </div>
+                <div class="col s1"></div>
 
                 </div>
             </div>
@@ -55,17 +57,25 @@
                     <div class=" col s12 m7">
                     <div class="card">
                         <div class="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div class="card-action">
-                        <a href="#">This is a link</a>
                         </div>
                     </div>
                     </div>
                 </div>
 
 
+        </div>
+        <div class="col s12">
+            <ul class="tabs">
+                <li class="tab col s3"><a href="#test1">
+                    <i class="medium material-icons">account_circle</i>
+                </a></li>
+                <li class="tab col s3"><a class="active" href="#test2">  
+                    <i class="medium material-icons">explore</i>
+                </a></li>
+                <li class="tab col s3"><a href="#test3">  
+                    <i class="medium material-icons">chat</i>
+                </a></li>
+            </ul>
         </div>
     </div>
 
