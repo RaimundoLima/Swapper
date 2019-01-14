6073d6d67d142b1e3340c76a5f51a16588053019
Jacinto <luishjacinto98@gmail.com>
2019-01-14 02:03:57 -0200

    HEHE

--- Swapper/View/footer.php
+++ Swapper/View/footer.php
@@ -1,7 +1,8 @@
 <footer>
     <div>
         <script src="js/jquery.js"></script>
-        <script src="js/materialize.js"></script>
+        <script src="js/materialize.js"></script>]
+        <script src="js/main.js"></script>
         <!--<script src="js/script.js"></script>-->
     </div>
 <footer>
