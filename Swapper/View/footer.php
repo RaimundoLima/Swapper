0a07784b325fbbdb6a56e48708250fce3ef0779b
Raimundo Lima <rai8991@hotmail.com>
2019-01-20 00:11:27 -0200

    swiper

--- Swapper/View/footer.php
+++ Swapper/View/footer.php
@@ -4,5 +4,6 @@
         <script src="View/js/materialize.js"></script>
         <script src="View/js/main.js"></script>
         <script src="View/js/chat.js"></script>
+        <script src="View/js/swiper.js"></script>
     </div>
 <footer>
--- Swapper/View/header.php
+++ Swapper/View/header.php
@@ -8,10 +8,14 @@
 
   <link type="text/css" rel="stylesheet" href="/View/css/style.css" />
 
+  <link type="text/css" rel="stylesheet" href="/View/css/swiper.css" />
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="description" content="Template maroto do site">
   <meta name="keywords" content="">
   <meta name="author" content="Raimundo Lima, Luis Jacinto, Bruno Ribeiro">
+
+
 </head>
 <body>
